1e56800481f0fb44401b6b4ec579f7b869fad6f4
mor683 <mor683@alumnos.unican.es>
2021-04-11 16:56:25 +0200

    Versión sin timer

--- practica3/src/main/java/es/unican/is2/Sonando.java
+++ practica3/src/main/java/es/unican/is2/Sonando.java
@@ -4,17 +4,18 @@
 
 package es.unican.is2;
 
-public class Sonando extends AlarmasState
+public class Sonando extends AlarmasState implements TimedState
 {
-	public void apagar( Alarmas context )
-	{
-		
-	}
+	TimedStateController controlador = TimedStateController.getInstance();
 	
-	public void borraAlarma( Alarmas context )
-	{
-		
+	public void apagar( Alarmas context ) {
+		controlador.cancel();
+		this.exitAction(context);
+		AlarmasState.getEstadoProgramado().entryAction(context);
+		AlarmasState.getEstadoProgramado().doAction(context);
+		context.setState(AlarmasState.getEstadoProgramado());
 	}
+<<<<<<< HEAD
 	
 	public void entryAction( Alarmas context )
 	{
@@ -24,7 +25,30 @@ public class Sonando extends AlarmasState
 	public void exitAction( Alarmas context )
 	{
 		// FALTA
+=======
+
+	/**public void borraAlarma( Alarmas context ) {
+		this.exitAction(context);								// desactivar melodia
+		AlarmasState.getEstadoProgramado().entryAction(context);
+		AlarmasState.getEstadoProgramado().doAction(context);
+		context.setState(AlarmasState.getEstadoProgramado());	// cambia a programado
+	}*/
+
+	public void entryAction(Alarmas context) {
+		controlador.startAbsolute(context, this, context.alarmaMasProxima().hora());
 	}
-	
-	
+
+	public void exitAction( Alarmas context ) {
+		context.desactivarMelodía();
+>>>>>>> 4a9cdceeb9e7a0970efe41f0acca94d74edfa4d9
+	}
+
+	public void timeout(Alarmas context) {
+		this.exitAction(context);								// desactivar melodia
+		AlarmasState.getEstadoProgramado().entryAction(context);
+		AlarmasState.getEstadoProgramado().doAction(context);
+		context.setState(AlarmasState.getEstadoProgramado());	// cambia a programado
+	}
+
+
 }
---
+++ practica3/src/main/java/es/unican/is2/TimedStateController.java
@@ -0,0 +1,44 @@
+package es.unican.is2;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimedStateController {
+
+	private static final TimedStateController controlador = new TimedStateController();
+	private Timer timer = new Timer();
+
+	public static TimedStateController getInstance() {
+		return controlador;
+	}
+
+	public void startRelative(Alarmas context, TimedState estado, int milis) {
+		timer.schedule(new Task(estado, context), milis);
+	}
+
+	public void startAbsolute(Alarmas context, TimedState estado, Date instante) {
+		timer.schedule(new Task(estado, context), instante);
+	}
+	
+	public void cancel() {
+		
+	}
+	
+	private class Task extends TimerTask {
+		TimedState timed;
+		Alarmas context;
+		
+		public Task(TimedState timed, Alarmas context) {
+			this.timed = timed;
+			this.context = context;
+		}
+
+		@Override
+		public void run() {
+			timed.timeout(context);			
+		}
+		
+	}
+
+}
